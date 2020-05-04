1b43bae2b2ebee427415239cb2e5f64158f8836b
Jean-Philippe Monette <contact@jpmonette.net>
2020-05-04 17:12:28 +0100

    Add "number" and "boolean" condition value types

--- src/__tests__/soql.spec.ts
+++ src/__tests__/soql.spec.ts
@@ -3,7 +3,7 @@ import SOQL from '../soql';
 describe('Query generator', () => {
   it('should generate simple query', () => {
     const query = new SOQL('Account').build();
-    expect(query).toBe(`SELECT Id FROM Account`);
+    expect(query).toMatchInlineSnapshot(`"SELECT Id FROM Account"`);
   });
 
   it('should add fields', () => {
@@ -11,16 +11,28 @@ describe('Query generator', () => {
       .select(['Id', 'Name', 'BillingCountry'])
       .select(['Id', 'BillingState'])
       .build();
-    expect(query).toBe(`SELECT Id, Name, BillingCountry, BillingState FROM Account`);
+    expect(query).toMatchInlineSnapshot(
+      `"SELECT Id, Name, BillingCountry, BillingState FROM Account"`
+    );
   });
 
   it('should add limit', () => {
     const query = new SOQL('Account').limit(10).build();
-    expect(query).toBe(`SELECT Id FROM Account LIMIT 10`);
+    expect(query).toMatchInlineSnapshot(`"SELECT Id FROM Account LIMIT 10"`);
   });
 
   it('should add where condition', () => {
     const query = new SOQL('Account').where('Id', '=', '001E000001KnMkTIAV').build();
-    expect(query).toBe(`SELECT Id FROM Account WHERE Id = '001E000001KnMkTIAV'`);
+    expect(query).toMatchInlineSnapshot(`"SELECT Id FROM Account WHERE Id = '001E000001KnMkTIAV'"`);
+  });
+
+  it('should add where condition boolean', () => {
+    const query = new SOQL('Account').where('IsPersonAccount', '=', true).build();
+    expect(query).toMatchInlineSnapshot(`"SELECT Id FROM Account WHERE IsPersonAccount = true"`);
+  });
+
+  it('should add where condition number', () => {
+    const query = new SOQL('Account').where('NumOfEmployees', '=', 30).build();
+    expect(query).toMatchInlineSnapshot(`"SELECT Id FROM Account WHERE NumOfEmployees = 30"`);
   });
 });
--- src/soql.ts
+++ src/soql.ts
@@ -2,7 +2,7 @@ export default class SOQL {
   fromText: string;
   fieldList: Set<string>;
   numberOfRows: number | undefined;
-  conditions: { field: string; operator: string; value: string }[];
+  conditions: { field: string; operator: string; value: string | boolean | number }[];
   numberOfRowsToSkip: number | undefined;
 
   constructor(sobject: string) {
@@ -21,18 +21,15 @@ export default class SOQL {
     return this;
   };
 
-  public where = (field: string, operator: string, value: string): SOQL => {
-    this.conditions.push({
-      field,
-      operator,
-      value,
-    });
+  public where = (field: string, operator: string, value: string | boolean | number): SOQL => {
+    this.conditions.push({ field, operator, value });
     return this;
   };
 
   public buildSelect = (): string => {
     if (this.fieldList.size !== 0) {
-      return 'SELECT ' + Array.from(this.fieldList).join(', ');
+      const fields = Array.from(this.fieldList).join(', ');
+      return `SELECT ${fields}`;
     } else {
       return 'SELECT Id';
     }
@@ -42,11 +39,17 @@ export default class SOQL {
     const condList: string[] = [];
 
     this.conditions.map(({ field, operator, value }) => {
-      condList.push(`${field} ${operator} '${value}'`);
+      let valueString: string;
+      if (typeof value === 'string') {
+        valueString = `'${value}'`;
+      } else {
+        valueString = value.toString();
+      }
+      condList.push(`${field} ${operator} ${valueString}`);
     });
 
     if (this.conditions.length !== 0) {
-      return 'WHERE ' + condList.join(' AND ');
+      return `WHERE ${condList.join(' AND ')}`;
     } else {
       return '';
     }
@@ -56,14 +59,14 @@ export default class SOQL {
     const queryParts: string[] = [];
 
     queryParts.push(this.buildSelect());
-    queryParts.push('FROM ' + this.fromText);
+    queryParts.push(`FROM ${this.fromText}`);
 
     if (this.conditions.length !== 0) {
       queryParts.push(this.buildConditions());
     }
 
     if (this.numberOfRows) {
-      queryParts.push('LIMIT ' + this.numberOfRows);
+      queryParts.push(`LIMIT ${this.numberOfRows}`);
     }
 
     return queryParts.join(' ');
